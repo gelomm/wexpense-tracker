3954aa514fc3ff50f628e65dddc2ca383658cdb2
gelomm <gelomendenilla@gmail.com>
2026-06-25 01:46:54 +0800

    workflow update for gh pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: '/wexpense-tracker/', // <-- same as your repo name
+  server: { port: 3000 },
 });
